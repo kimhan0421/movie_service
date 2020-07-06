6350811a87eea85598043cd9c9b9598b76cbe448
kimhansol <kimhan0421@naver.com>
2020-07-06 21:58:04 +0900

    Updates

---
+++ precache-manifest.103687591631ab8465766b74818e7e4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4bd968d1dc5bde6ebafeb277390560a",
+    "url": "/movie_service/index.html"
+  },
+  {
+    "revision": "f162a29fc2a3089c1d89",
+    "url": "/movie_service/static/css/main.2026eb9d.chunk.css"
+  },
+  {
+    "revision": "1fbe4910888ebe891b23",
+    "url": "/movie_service/static/js/2.2ead458c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_service/static/js/2.2ead458c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f162a29fc2a3089c1d89",
+    "url": "/movie_service/static/js/main.31424fc5.chunk.js"
+  },
+  {
+    "revision": "24500552998d4b1cdc40",
+    "url": "/movie_service/static/js/runtime-main.ad39f912.js"
+  }
+]);
